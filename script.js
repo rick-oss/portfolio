df45f23865ccb0f0f3583926a88d04f00189638e
Erick <erickenedeno290@gmail.com>
2023-10-26 16:23:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// Codigo lightbox 
+document.addEventListener("DOMContentLoaded", function () {
+    // Funçao para abrir o lightbox
+    function openLightboxFromThumbnail(thumbnail) {
+        const lightbox = document.getElementById('lightbox');
+        const imageSrc = thumbnail.getAttribute('data-src');
+
+        // Atribui o caminho da imagem à imagem do lightbox
+        document.getElementById('lightboxImagem').src = imageSrc;
+
+        // mostra o lightbox
+        lightbox.style.display = 'block';
+    }
+
+    // Funçao para fechar o lightbox 
+    function closeLightbox() {
+        const lightbox = document.getElementById('lightbox');
+        lightbox.style.display = 'none';
+    }
+
+    // Adiciona um evento de clique às miniaturas para abrir o lightbox
+    const thumbnails = document.querySelectorAll('.images');
+
+    thumbnails.forEach(function (thumbnail) {
+        thumbnail.addEventListener('click', function () {
+            openLightboxFromThumbnail(thumbnail);
+        });
+    });
+
+    // Adiciona um evento de click a sobreposiçao do lightbox para fechar o lightbox
+    const lightbox = document.getElementById('lightbox');
+    lightbox.addEventListener('click', closeLightbox);
+});
+
+// Codigo mostrar mais
+document.addEventListener("DOMContentLoaded", function () {
+    const showMoreButoon = document.getElementById('show-more')
+    const detalhesProjeto = document.querySelector('.detalhes-projeto')
+
+    showMoreButoon.addEventListener('click', () => {
+        if (detalhesProjeto.style.maxHeight) {
+            detalhesProjeto.style.maxHeight = null;
+            showMoreButoon.textContent = '▼ Mostrar Mais';
+        } else {
+            detalhesProjeto.style.maxHeight =  detalhesProjeto.scrollHeight + 'px';
+            showMoreButoon.textContent = '▲ Mostrar Menos'
+        }
+    });
+});
+  

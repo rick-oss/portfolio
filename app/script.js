27000b446d287e122fd2678fdd95d54776824fd0
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2024-07-27 18:49:26 -0300

    Atualiza configuraçoes e links para hospedagem

--- app/script.js
+++ app/script.js
@@ -1,4 +1,3 @@
-// Codigo lightbox 
 document.addEventListener("DOMContentLoaded", function () {
     // Funçao para abrir o lightbox
     function openLightboxFromThumbnail(thumbnail) {
@@ -19,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Adiciona um evento de clique às miniaturas para abrir o lightbox
-    const thumbnails = document.querySelectorAll('.images');
+    const thumbnails = document.querySelectorAll('.image img');
 
     thumbnails.forEach(function (thumbnail) {
         thumbnail.addEventListener('click', function () {
@@ -30,12 +29,11 @@ document.addEventListener("DOMContentLoaded", function () {
     // Adiciona um evento de click a sobreposiçao do lightbox para fechar o lightbox
     const lightbox = document.getElementById('lightbox');
     lightbox.addEventListener('click', closeLightbox);
-});
 
-// Codigo mostrar mais
-document.addEventListener("DOMContentLoaded", function () {
+
+    // Código mostrar mais:
     const showMoreButtons = document.querySelectorAll('.show-more');
-    const detalhesProjetos = document.querySelectorAll('.detalhes-projeto');
+    const detalhesProjetos = document.querySelectorAll('.details-project');
 
     // Adiciona um evento de click a todos os botões mostrar mais
     showMoreButtons.forEach((button, index) => {
@@ -52,4 +50,50 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+
+    // Código Typed:
+    var typed = new Typed(".typed-text", {
+        strings: ["Desenvolver Web Frontend"],
+        typeSpeed: 120,
+        backSpeed: 100,
+        backDelay: 1000,
+    });
+
+
+    // recupera todas os links no navbar
+    const navlinks = document.querySelectorAll('.navbar a');
+    const sections = document.querySelectorAll('section');
+    let isScrollingByLink = false
+
+    // percorre todas os links e adicionar um evento de clique a cada um
+    navlinks.forEach(link => {
+        link.addEventListener('click', function () {
+            // remove a classe active de todos os links
+            navlinks.forEach(item => item.classList.remove('active'))
+
+            // Adiciona a classe active ao link clicado
+            this.classList.add('active')
+
+            isScrollingByLink = true;
+            setTimeout(() => {
+                isScrollingByLink = false
+                console.log(`${isScrollingByLink}`)
+            }, 1000);
+        })
+    })
+
+
+    // Código de automatizaçao de active menu de navegaçao:
+    function changeActiveLink() {
+        if (isScrollingByLink) return; // Se a flag for true, interrompe a função
+        let index = sections.length;
+
+        while (--index && window.scrollY + 100 < sections[index].offsetTop) { }
+
+        navlinks.forEach((link) => link.classList.remove('active'));
+        navlinks[index].classList.add('active');
+    }
+
+    changeActiveLink();
+    window.addEventListener('scroll', changeActiveLink);
 });

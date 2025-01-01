72243f742166bde828e61357ea5b84526f7f5bc3
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-01-01 17:49:05 -0300

    Monta todo  componente usando os dados dinâmicos vindos de ProjectCard

--- src/components/layout/Modal.jsx
+++ src/components/layout/Modal.jsx
@@ -3,12 +3,10 @@ import ReactDom from "react-dom";
 import { FaX } from "react-icons/fa6";
 
 import styles from "./Modal.module.css";
-import imagem from "./../../assets/conselho.png";
+// import imagem from "./../../assets/conselho.png";
 
-function Modal({ children, isOpen, onClose, titulo }) {
+function Modal({ isOpen, onClose, title, description, image, date, links }) {
   if (!isOpen) return null;
-
-  console.log(isOpen);
 
   return ReactDom.createPortal(
     <div className={styles.modal}>
@@ -16,25 +14,24 @@ function Modal({ children, isOpen, onClose, titulo }) {
         <div className={styles.main_content}>
           <div className={styles.video_wrapper}>
             {/*video*/}
-            <img src={imagem} />
+            <img src={image} />
           </div>
-
           <div className={styles.project_details}>
             <div className={styles.title_wrapper}>
-              <h2>{titulo}</h2>
+              <h2>{title}</h2>
               <button className={styles.close_button} onClick={onClose}>
                 <FaX className={styles.icon_button} />
               </button>
             </div>
-            <p className={styles.text}>{children}</p>
-            <p className={styles.date}>11 de Novembro, 2023</p>
+            <p className={styles.text}>{description}</p>
+            <p className={styles.date}>{date}</p>
           </div>
         </div>
         <div className={styles.buttons_wrapper}>
-          <a href="https://github.com/rick-oss" target="_blank" rel="noopener noreferrer">
+          <a href={links} target="_blank" rel="noopener noreferrer">
             Ver Repositório
           </a>
-          <a href="https://github.com/rick-oss" target="_blank" rel="noopener noreferrer">
+          <a href={links} target="_blank" rel="noopener noreferrer">
             Github
           </a>
         </div>
@@ -45,11 +42,13 @@ function Modal({ children, isOpen, onClose, titulo }) {
 }
 
 Modal.propTypes = {
-  children: PropTypes.node.isRequired,
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
-  titulo: PropTypes.string.isRequired,
-  video: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.object.isRequired,
+  image: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  links: PropTypes.string.isRequired,
 };
 
 export default Modal;

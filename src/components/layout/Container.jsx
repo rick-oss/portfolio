6b637203d8eed8293753bfb26c387a5f37691868
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-01-03 17:19:42 -0300

    Apaga código desnecessário

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -2,16 +2,11 @@ import PropTypes from "prop-types";
 
 import styles from "./Container.module.css";
 
-function Container({ children, id, customClass }) {
-  return (
-    <div id={id} className={`${styles.container} ${styles[customClass]}`}>
-      {children}
-    </div>
-  );
+function Container({ children, customClass }) {
+  return <div className={`${styles.container} ${styles[customClass]}`}>{children}</div>;
 }
 
 Container.propTypes = {
-  id: PropTypes.string.isRequired,
   customClass: PropTypes.string,
   children: PropTypes.node.isRequired,
 };

fad5e615d06c88b72c596cd3e38cfd0c2b6cf312
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-01-03 17:30:47 -0300

    Importa Element nos componentes para a implementaçao do react-scroll

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,3 +1,5 @@
+import { Element } from "react-scroll";
+
 import Container from "../layout/Container";
 import Title from "../layout/Title";
 import ProjectCard from "../layout/ProjectCard";
@@ -7,16 +9,18 @@ import styles from "./Projects.module.css";
 
 function Projects() {
   return (
-    <Container id="Projects">
-      <div className={styles.projects_content}>
-        <Title blueText="Proje" whiteText="Tos" />
-        <div className={styles.items}>
-          {projectsData.map((project) => (
-            <ProjectCard project={project} key={project.id} />
-          ))}
+    <Element name="projects">
+      <Container>
+        <div className={styles.projects_content}>
+          <Title blueText="Proje" whiteText="Tos" />
+          <div className={styles.items}>
+            {projectsData.map((project) => (
+              <ProjectCard project={project} key={project.id} />
+            ))}
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </Element>
   );
 }
 
--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -1,4 +1,5 @@
 import { FaHtml5, FaCss3Alt, FaReact, FaGitAlt, FaPython, FaJs } from "react-icons/fa";
+import { Element } from "react-scroll";
 
 import Container from "../layout/Container";
 import SkillCard from "../layout/SkillCard";
@@ -8,19 +9,21 @@ import styles from "./Skills.module.css";
 
 function Skills() {
   return (
-    <Container id="Skills">
-      <div className={styles.skills_content}>
-        <Title blueText="Tecno" whiteText="Logias" />
-        <div className={styles.items}>
-          <SkillCard skillIcon={<FaHtml5 />} skillName="HTML5" />
-          <SkillCard skillIcon={<FaCss3Alt />} skillName="CSS3" />
-          <SkillCard skillIcon={<FaJs />} skillName="javaScript" />
-          <SkillCard skillIcon={<FaReact />} skillName="React" />
-          <SkillCard skillIcon={<FaGitAlt />} skillName="Git" />
-          <SkillCard skillIcon={<FaPython />} skillName="Python" />
+    <Element name="skills">
+      <Container>
+        <div className={styles.skills_content}>
+          <Title blueText="Tecno" whiteText="Logias" />
+          <div className={styles.items}>
+            <SkillCard skillIcon={<FaHtml5 />} skillName="HTML5" />
+            <SkillCard skillIcon={<FaCss3Alt />} skillName="CSS3" />
+            <SkillCard skillIcon={<FaJs />} skillName="javaScript" />
+            <SkillCard skillIcon={<FaReact />} skillName="React" />
+            <SkillCard skillIcon={<FaGitAlt />} skillName="Git" />
+            <SkillCard skillIcon={<FaPython />} skillName="Python" />
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </Element>
   );
 }
 

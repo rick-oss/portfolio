c599a02df3f002c5631fc8427e7c20a54ed962ca
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-01-03 17:29:28 -0300

    Importa Element no componente para implementar o react-scroll

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { useEffect, useRef } from "react";
+import { Element } from "react-scroll";
 import Typed from "typed.js";
 
 import Container from "../layout/Container";
@@ -25,33 +26,35 @@ function Home() {
   }, []);
 
   return (
-    <Container id="home" customClass="justify_between">
-      <div className={styles.home_content}>
-        <h3>Olá, Eu sou</h3>
-        <h1>Erick Nunes</h1>
-        <h3>
-          E sou <span ref={typedElement}></span>
-        </h3>
-        <div className={styles.links}>
-          <a href="https://github.com/rick-oss" target="_blank" rel="noopener noreferrer">
-            <FaGithub />
-          </a>
+    <Element name="home">
+      <Container customClass="justify_between">
+        <div className={styles.home_content}>
+          <h3>Olá, Eu sou</h3>
+          <h1>Erick Nunes</h1>
+          <h3>
+            E sou <span ref={typedElement}></span>
+          </h3>
+          <div className={styles.links}>
+            <a href="https://github.com/rick-oss" target="_blank" rel="noopener noreferrer">
+              <FaGithub />
+            </a>
 
-          <a href="http://www.linkedin.com/in/erick-nunes-3a1274287" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin />
+            <a href="http://www.linkedin.com/in/erick-nunes-3a1274287" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin />
+            </a>
+          </div>
+          <a
+            href="https://drive.google.com/uc?export=download&id=1IM_M_HpweO0n8HHJNzlb9O9z7NElBlIR"
+            download
+            className={styles.btn}
+          >
+            Download CV
           </a>
         </div>
-        <a
-          href="https://drive.google.com/uc?export=download&id=1IM_M_HpweO0n8HHJNzlb9O9z7NElBlIR"
-          download
-          className={styles.btn}
-        >
-          Download CV
-        </a>
-      </div>
 
-      <img src={eu} alt="Eu menó" className={styles.image} />
-    </Container>
+        <img src={eu} alt="Eu menó" className={styles.image} />
+      </Container>
+    </Element>
   );
 }
 

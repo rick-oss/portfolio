468ae16990e16a573705470c6d3feb8ed8e83df8
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-06-19 17:19:12 -0300

    feat: adiciona transições ao entrar na seção com intersection observer

--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -1,4 +1,5 @@
 import { FaHtml5, FaCss3Alt, FaReact, FaGitAlt, FaPython, FaJs } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
 import { Element } from "react-scroll";
 
 import Container from "../layout/Container";
@@ -8,12 +9,25 @@ import Title from "../layout/Title";
 import styles from "./Skills.module.css";
 
 function Skills() {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0.3,
+  });
+
   return (
     <Element name="skills">
       <Container>
         <div className={styles.skills_content}>
           <Title blueText="Tecno" whiteText="Logias" />
-          <div className={styles.items}>
+          <div
+            ref={ref}
+            className={styles.items}
+            style={{
+              transition: "all 0.5s ease",
+              opacity: inView ? 1 : 0,
+              transform: inView ? "translateX(0)" : "translateX(-60px)",
+            }}
+          >
             <SkillCard skillIcon={<FaHtml5 />} skillName="HTML5" />
             <SkillCard skillIcon={<FaCss3Alt />} skillName="CSS3" />
             <SkillCard skillIcon={<FaJs />} skillName="JavaScript" />

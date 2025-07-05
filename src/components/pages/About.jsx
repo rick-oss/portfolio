cf42f30ced9d51745586ebf304276f6aa7faf2df
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-07-05 19:25:35 -0300

    chore: atualiza texto da seção sobre mim

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -13,14 +13,14 @@ function About() {
           <Title blueText="Sobre" whiteText="Mim" />
           <div className={styles.text}>
             <p>
-              Olá, tenho 20 anos e sou um desenvolvedor web, sempre em busca de inovação e aprendizado. Atualmente faço
+              Olá, tenho 21 anos e sou um desenvolvedor web, sempre em busca de inovação e aprendizado. Atualmente faço
               projetos com <span>HTML</span>, <span>CSS</span> e <span>Javascript</span>. Adoro desenhar e aprender
-              coisas novas no meu tempo livre, além de apaixonado por tecnologia, historias e jogos. Entrei em contato
-              com esse mundo aos 16 anos, com a linguagem <span>Python</span>, onde desenvolvi todas as minhas bases de
-              conhecimentos, conceitos e lógica. Desenvolvi alguns pequenos projetos utilizando <span>Flask</span>, e
-              nesses projetos criei uma enorme curiosidade com toda a parte do front-end. Desde então, sigo aprendendo e
-              aprimorando ainda mais as minhas habilidades nessas tecnologias. Além de seguir estudando e buscando
-              aprender mais as tecnologias já citadas, atualmente estou estudando <span>Node.js</span> e a biblioteca{" "}
+              coisas novas no meu tempo livre, além de ser apaixonado por tecnologia, historias e jogos. Entrei em
+              contato com esse mundo aos 16 anos, com a linguagem <span>Python</span>, onde desenvolvi todas as minhas
+              bases de conhecimentos, conceitos e lógica. Desenvolvi alguns pequenos projetos utilizando{" "}
+              <span>Flask</span>, e nesses projetos criei uma enorme curiosidade com toda a parte do front-end. Desde
+              então, sigo aprendendo e aprimorando ainda mais as minhas habilidades nessas tecnologias. Além de seguir
+              estudando e buscando aprender mais as tecnologias já citadas, atualmente estou estudando a biblioteca{" "}
               <span>React.js</span>. Ademais, estou sempre em busca de novos desafios e oportunidades para aprimorar
               minhas habilidades, por isso, fique à vontade para explorar meus projetos e entrar em contato comigo.
             </p>

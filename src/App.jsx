a0943f72e69dbf70cf171781be7fe5e6bffe86bf
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-08-03 18:21:11 -0300

    chore: atualiza o caminho das importações para refletir a mudança de pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Skills from "./components/pages/Skills";
-import Projects from "./components/pages/Projects";
+import Home from "./sections/Home";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
 
 import Navbar from "./components/layout/Navbar";
 
